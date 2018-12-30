0193693f70c762925f2a512a45ccc5d61e9aaaf4
goyslee <goyslee@gmail.com>
2018-12-30 11:15:24 +0000

    default skill update final

--- public/allocation.js
+++ public/allocation.js
@@ -1,104 +1,77 @@
-
-
-$('td:contains("$init")').each(function(){
+$('td:contains("$init")').each(function() {
     $(this).html($(this).html().split("$init").join("<span hidden='true'>$init</span>"));
 });
-
-$('td:contains("|")').each(function(){
+$('td:contains("|")').each(function() {
     $(this).html($(this).html().split("|").join("-"));
 });
-
-$('td:contains("chutes")').each(function(){
+$('td:contains("chutes")').each(function() {
     $(this).html($(this).html().split("chutes").join("Chill Pick"));
 });
-
-$('td:contains("mts")').each(function(){
+$('td:contains("mts")').each(function() {
     $(this).html($(this).html().split("mts").join("De-Kit"));
 });
-
-$('td:contains("trucks")').each(function(){
+$('td:contains("trucks")').each(function() {
     $(this).html($(this).html().split("trucks").join("Reach Truck"));
 });
-
-
-$('td:contains("manual_pick")').each(function(){
+$('td:contains("manual_pick")').each(function() {
     $(this).html($(this).html().split("manual_pick").join("Checking"));
 });
-
-
-
-
 // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
 $(window).on("load resize ", function() {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-  $('.tbl-header').css({'padding-right':scrollWidth});
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({
+        'padding-right': scrollWidth
+    });
 }).resize();
-
-
-
 $("check").on('change', function() {
-  if ($(this).is(':checked')) {
-    $(this).attr('value', 'true');
-     
-  } else {
-    $(this).attr('value', 'false');
-    
-  }
-  
- });
-
-$("check").is(':checked', function(){
-              $("check").attr('value', 'true');
-          });
-
+    if ($(this).is(':checked')) {
+        $(this).attr('value', 'true');
+    } else {
+        $(this).attr('value', 'false');
+    }
+});
+$("check").is(':checked', function() {
+    $("check").attr('value', 'true');
+});
 $("form").submit(function() {
     $("input").removeAttr("disabled");
 });
-
 function check() {
     document.getElementByClassName("check").checked = true;
 }
-
 function uncheck() {
     document.getElementByClassName("check").unchecked = false;
 }
-
 var rotas = {
-  earlies: {
-    w1: [0,1,1,1,1,0,1],
-    w2: [1,1,1,1,0,1,1],
-    w3: [1,0,0,1,1,1,1],
-    w4: [0,0,1,1,1,1,0],
-    w5: [0,1,1,1,0,1,1],
-    w6: [1,1,1,0,1,1,1],
-    w7: [1,1,0,0,0,1,1],
-    w8: [0,1,1,1,1,0,0]
-  },
-
-  middles: {
-    w1: [0,1,1,1,1,0,0],
-    w2: [1,1,1,1,0,1,1],
-    w3: [1,0,0,1,1,1,1],
-    w4: [1,0,1,1,1,1,0],
-    w5: [0,1,1,1,0,1,1],
-    w6: [1,1,0,0,1,1,1],
-    w7: [1,1,0,0,1,1,1],
-    w8: [1,0,1,1,1,1,0] 
-  },
-
-  nights: {
-    w1: [1,1,1,0,0,1,1],
-    w2: [1,1,0,0,1,1,1],
-    w3: [1,0,0,1,1,1,0],
-    w4: [0,1,1,1,1,0,0],
-    w5: [1,1,1,0,0,1,1],
-    w6: [1,0,0,1,1,1,1],
-    w7: [1,0,0,1,1,1,0],
-    w8: [0,1,1,1,1,0,0]
-  }
+    earlies: {
+        w1: [0, 1, 1, 1, 1, 0, 1],
+        w2: [1, 1, 1, 1, 0, 1, 1],
+        w3: [1, 0, 0, 1, 1, 1, 1],
+        w4: [0, 0, 1, 1, 1, 1, 0],
+        w5: [0, 1, 1, 1, 0, 1, 1],
+        w6: [1, 1, 1, 0, 1, 1, 1],
+        w7: [1, 1, 0, 0, 0, 1, 1],
+        w8: [0, 1, 1, 1, 1, 0, 0]
+    },
+    middles: {
+        w1: [0, 1, 1, 1, 1, 0, 0],
+        w2: [1, 1, 1, 1, 0, 1, 1],
+        w3: [1, 0, 0, 1, 1, 1, 1],
+        w4: [1, 0, 1, 1, 1, 1, 0],
+        w5: [0, 1, 1, 1, 0, 1, 1],
+        w6: [1, 1, 0, 0, 1, 1, 1],
+        w7: [1, 1, 0, 0, 1, 1, 1],
+        w8: [1, 0, 1, 1, 1, 1, 0]
+    },
+    nights: {
+        w1: [1, 1, 1, 0, 0, 1, 1],
+        w2: [1, 1, 0, 0, 1, 1, 1],
+        w3: [1, 0, 0, 1, 1, 1, 0],
+        w4: [0, 1, 1, 1, 1, 0, 0],
+        w5: [1, 1, 1, 0, 0, 1, 1],
+        w6: [1, 0, 0, 1, 1, 1, 1],
+        w7: [1, 0, 0, 1, 1, 1, 0],
+        w8: [0, 1, 1, 1, 1, 0, 0]
+    }
 }
-
-
 // -------------------------------------------------------------------------------------------------
-
-
